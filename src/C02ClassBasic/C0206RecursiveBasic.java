03ace046e4801d0e9405215c13f2a865c9a43404
suhyunpark <psh7492@gmail.com>
2024-06-13 14:05:54 +0900

    1부터 10까지의 덧셈 재귀문

--- src/C02ClassBasic/C0206RecursiveBasic.java
+++ src/C02ClassBasic/C0206RecursiveBasic.java
@@ -2,9 +2,21 @@ package C02ClassBasic;
 
 public class C0206RecursiveBasic {
 	public static void main(String[] args) {
-		System.out.println("main함수 시작");
-		function1();
-		System.out.println("main함수 끝");
+		// for문으로 1~10까지 누적합계
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		System.out.println(addAcc(10));
+	}
+
+	static int addAcc(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		System.out.println("n : " + n);
+		return n + addAcc(n - 1);
 	}
 
 	static void function1() {
