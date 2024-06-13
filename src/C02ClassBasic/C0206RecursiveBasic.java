7a1b1e7bd424582f9db0a498445e05321b92a794
suhyunpark <psh7492@gmail.com>
2024-06-13 14:26:34 +0900

    피보나치 수열 재귀 예시

--- src/C02ClassBasic/C0206RecursiveBasic.java
+++ src/C02ClassBasic/C0206RecursiveBasic.java
@@ -1,5 +1,7 @@
 package C02ClassBasic;
 
+import java.util.Arrays;
+
 public class C0206RecursiveBasic {
 	public static void main(String[] args) {
 		// for문으로 1~10까지 누적합계
@@ -7,8 +9,25 @@ public class C0206RecursiveBasic {
 		for (int i = 1; i <= 10; i++) {
 			sum += i;
 		}
-		System.out.println(sum);
-		System.out.println(addAcc(10));
+		// System.out.println(sum);
+		// System.out.println(addAcc(10));
+
+
+	//	피보나치 수열
+	//	재귀문 이용
+		long first = 1;
+		long second = 1;
+		long total = first + second;
+		for (int i = 0; i <= 3; i++) {
+			first = second;
+			second = total;
+			total = first + second;
+			// System.out.println("total: " + total);
+		}
+		System.out.println(total);
+
+	//	재귀문 이용
+		System.out.println(fibonacci(5));
 	}
 
 	static int addAcc(int n) {
@@ -18,6 +37,15 @@ public class C0206RecursiveBasic {
 		System.out.println("n : " + n);
 		return n + addAcc(n - 1);
 	}
+
+	// 재귀문으로 처리하면 중복으로 계산해야하는 부분이 너무 많음
+	static int fibonacci(int n){
+		if (n <= 2) {
+			return 1;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
 
 	static void function1() {
 		System.out.println("function1 시작");
