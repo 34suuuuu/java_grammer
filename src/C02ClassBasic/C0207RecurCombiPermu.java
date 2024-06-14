ca951ea891359454de64e2134fbfb78cc05ca3dd
suhyunpark <psh7492@gmail.com>
2024-06-14 11:37:33 +0900

    순열 조합 재귀문 수정

--- src/C02ClassBasic/C0207RecurCombiPermu.java
+++ src/C02ClassBasic/C0207RecurCombiPermu.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class C0207RecurCombiPermu {
+
 	static List<List<Integer>> answer;
 	static List<Integer> myList;
 	static int cnt;
@@ -12,22 +13,22 @@ public class C0207RecurCombiPermu {
 	public static void main(String[] args) {
 
 		// List<Integer> myList = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-
-	//	myList로 만들 수 있는 2개짜리 숫자 조합을 2중 리스트에 담기
-	// 	List<Integer[]> result = new ArrayList<>();
-	// 	List<List<Integer>> answer = new ArrayList<>();
-	// 	List<Integer> tmp = new ArrayList<>();
-	//
-	// 	int cnt = 0;
-	// 	for (int i = 0; i < myList.size()-1; i++) {
-	// 		tmp.add(myList.get(i));
-	// 		for (int j = i + 1; j < myList.size(); j++) {
-	// 			tmp.add(myList.get(j));
-	// 			comb(myList, answer, tmp, cnt, i);
-	// 			tmp.remove(myList.size() - 1);
-	// 		}
-	// 		tmp.remove(myList.size() - 1);
-	// 	}
+		//
+		// myList로 만들 수 있는 2개짜리 숫자 조합을 2중 리스트에 담기
+		// List<Integer[]> result = new ArrayList<>();
+		// List<List<Integer>> answer = new ArrayList<>();
+		// List<Integer> tmp = new ArrayList<>();
+		//
+		// int cnt = 0;
+		// for (int i = 0; i < myList.size()-1; i++) {
+		// 	tmp.add(myList.get(i));
+		// 	for (int j = i + 1; j < myList.size(); j++) {
+		// 		tmp.add(myList.get(j));
+		// 		comb(myList, answer, tmp, cnt, i);
+		// 		tmp.remove(myList.size() - 1);
+		// 	}
+		// 	tmp.remove(myList.size() - 1);
+		// }
 
 		myList = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 		answer = new ArrayList<>();
