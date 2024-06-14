c02c4707a782dbd55a8759f6f91fdf70e654b056
suhyunpark <psh7492@gmail.com>
2024-06-14 12:10:10 +0900

    static final 키워드

---
+++ src/C04Interface/StaticFinalInterface.java
@@ -0,0 +1,25 @@
+package C04Interface;
+
+public interface StaticFinalInterface {
+	public static void main(String[] args) {
+
+		MyImplements m1 = new MyImplements();
+		m1.methodA();
+		//MyImplements에 my_constant변수가 상속(static final)
+		System.out.println();
+	}
+}
+
+interface  MyInterface{
+	//	아래 변수는 컴파일타임에 static final키워드가 붙음
+	int my_constant = 10;
+}
+
+class MyImplements implements MyInterface{
+
+	public void methodA(){
+		System.out.println("methodA");
+
+	}
+
+}
